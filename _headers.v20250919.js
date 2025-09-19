70c0ea2cac5f3016cdbbe1809c7d6d33f537963e
tomglo10 <153346363+tomglo10@users.noreply.github.com>
2025-09-19 18:01:18 -0500

    Update _headers.v20250919.js

--- _headers.v20250919.js
+++ _headers.v20250919.js
@@ -1,4 +1,22 @@
 /assets/*
   Access-Control-Allow-Origin: *
   Cache-Control: public, max-age=31536000
+<script>
+    // Create meta tags
+    const metaCacheControl = document.createElement('meta');
+    metaCacheControl.setAttribute('http-equiv', 'Cache-Control');
+    metaCacheControl.setAttribute('content', 'public, max-age=30d');
 
+    const metaPragma = document.createElement('meta');
+    metaPragma.setAttribute('http-equiv', 'Pragma');
+    metaPragma.setAttribute('content', 'cache');
+
+    const metaExpires = document.createElement('meta');
+    metaExpires.setAttribute('http-equiv', 'Expires');
+    metaExpires.setAttribute('content', 'Thu, 21 Oct 2027 07:28:00 GMT');
+
+    // Append meta tags to the head
+    document.head.appendChild(metaCacheControl);
+    document.head.appendChild(metaPragma);
+    document.head.appendChild(metaExpires);
+</script>
